125367be360c1d38cf556b7bb604f8a35ce80e28
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-10-21 15:56:11 +0200

    initialisation projet Voiture Deluxe

---
+++ VoitureFront/src/main/java/voitureDeluxe/VoitureDeluxeApplication.java
@@ -0,0 +1,13 @@
+package voitureDeluxe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoitureDeluxeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoitureDeluxeApplication.class, args);
+	}
+
+}
---
+++ VoitureFront/src/test/java/voitureDeluxe/VoitureDeluxeApplicationTests.java
@@ -0,0 +1,13 @@
+package voitureDeluxe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoitureDeluxeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
