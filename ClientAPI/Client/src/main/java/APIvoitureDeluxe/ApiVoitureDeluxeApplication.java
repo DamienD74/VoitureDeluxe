6c931085be9ae13661b40131c1c85d7403d6565b
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-10-21 16:09:55 +0200

    Ajout base API Client et Location

---
+++ ClientAPI/Client/src/main/java/APIvoitureDeluxe/ApiVoitureDeluxeApplication.java
@@ -0,0 +1,13 @@
+package APIvoitureDeluxe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVoitureDeluxeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVoitureDeluxeApplication.class, args);
+	}
+
+}
