d7642b3771d3148189b11cb43f86a27905aa41d7
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-10-22 16:05:34 +0200

    début structure index import css et image

--- VoitureFront/src/main/java/voitureDeluxe/Controller/MainController.java
+++ VoitureFront/src/main/java/voitureDeluxe/Controller/MainController.java
@@ -1,16 +1,26 @@
 package voitureDeluxe.Controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import voitureDeluxe.model.Voiture;
 
 @Controller
 public class MainController {
     
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
 	public String index(Model model) {
-
+        
+		String url = "http://localhost:8082/voitures";
+        RestTemplate restTemplate = new RestTemplate();
+        List<Voiture> voitureList = restTemplate.getForObject(url, List.class);
+        model.addAttribute("voitures",voitureList);
+		
 		return "index";
 	}
 }
--- VoitureFront/src/main/java/voitureDeluxe/VoitureDeluxeApplication.java
+++ VoitureFront/src/main/java/voitureDeluxe/VoitureDeluxeApplication.java
@@ -9,5 +9,4 @@ public class VoitureDeluxeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VoitureDeluxeApplication.class, args);
 	}
-
 }
